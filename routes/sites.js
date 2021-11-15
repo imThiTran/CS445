e52da7fab8d5bcad0871df2d790d93686449c34c
imThiTran <tranvanthitk113@gmail.com>
2021-11-15 20:27:49 +0700

    thay doi ngay thang theo tuan hien tai thoi gian thuc

--- routes/sites.js
+++ routes/sites.js
@@ -10,6 +10,14 @@ var Film = require('../models/film');
 
 router.get('/',function(req,res){
     var today = new Date();
+    var dayWeek = [];
+    var daylength=24*60*60*1000;
+    var monday=today.getTime()-(today.getDay()-1)*daylength;
+    for (var i=0;i<7;i++){
+        var newMonday= new Date(monday);
+        dayWeek.push(newMonday);
+        monday=monday+daylength;
+    }
     var dateRq=today.getDate()+'/'+(today.getMonth()+1)+'/'+today.getFullYear();
     Film.find({status:"showing"},function(err,fis){
         Film.find({status:"coming"},function(err,fic){
@@ -17,7 +25,8 @@ router.get('/',function(req,res){
         res.render('index',{
             films:fis,
             filmc:fic,
-            dateRq:dateRq
+            dateRq:dateRq,
+            dayWeek: dayWeek
             });
         })
     })
