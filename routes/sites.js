72455146520568b7cb79df9ab2b8a134d3f82b43
imThiTran <tranvanthitk113@gmail.com>
2021-11-15 17:26:07 +0700

    sua lai db va hiển thị phim theo ngày hiện tại thời gian thực

--- routes/sites.js
+++ routes/sites.js
@@ -9,12 +9,15 @@ var Film = require('../models/film');
 
 
 router.get('/',function(req,res){
+    var today = new Date();
+    var dateRq=today.getDate()+'/'+(today.getMonth()+1)+'/'+today.getFullYear();
     Film.find({status:"showing"},function(err,fis){
         Film.find({status:"coming"},function(err,fic){
-        if (err) return console.log(err);
+            if (err) return console.log(err);
         res.render('index',{
             films:fis,
             filmc:fic,
+            dateRq:dateRq
             });
         })
     })
