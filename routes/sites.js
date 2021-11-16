f20ddbf1ffd34aaf4e9937838a0d4458fcc564d1
imThiTran <tranvanthitk113@gmail.com>
2021-11-16 09:19:16 +0700

    chỉ hiện thị phim có suất chiếu trong ngày được chọn

--- routes/sites.js
+++ routes/sites.js
@@ -13,23 +13,63 @@ router.get('/',function(req,res){
     var dayWeek = [];
     var daylength=24*60*60*1000;
     var monday=today.getTime()-(today.getDay()-1)*daylength;
-    for (var i=0;i<7;i++){
+    for (var i=0;i<14;i++){
         var newMonday= new Date(monday);
         dayWeek.push(newMonday);
         monday=monday+daylength;
     }
     var dateRq=today.getDate()+'/'+(today.getMonth()+1)+'/'+today.getFullYear();
+    var newFis=[];
     Film.find({status:"showing"},function(err,fis){
+        for(var i=0;i<fis.length;i++){
+            for(var j=0;j<fis[i].showtime.length;j++){
+                if (fis[i].showtime[j].date==dateRq) newFis.push(fis[i]);
+            }
+        }
         Film.find({status:"coming"},function(err,fic){
             if (err) return console.log(err);
-        res.render('index',{
-            films:fis,
+            res.render('index',{
+            filmslide:fis,
+            films:newFis,
             filmc:fic,
             dateRq:dateRq,
             dayWeek: dayWeek
             });
         })
     })
+})
+
+router.get('/:time',function(req,res){
+    var today = new Date();
+    var time=new Date(parseInt(req.params.time));
+    if (time.getDate()==(today.getDate())) return res.redirect('/');
+        var dayWeek = [];
+        var daylength=24*60*60*1000;
+        var monday=today.getTime()-(today.getDay()-1)*daylength;
+        for (var i=0;i<14;i++){
+            var newMonday= new Date(monday);
+            dayWeek.push(newMonday);
+            monday=monday+daylength;
+        }
+        var dateRq=time.getDate()+'/'+(time.getMonth()+1)+'/'+time.getFullYear();
+        var newFis=[];
+        Film.find({status:"showing"},function(err,fis){
+            for(var i=0;i<fis.length;i++){
+                for(var j=0;j<fis[i].showtime.length;j++){
+                    if (fis[i].showtime[j].date==dateRq) newFis.push(fis[i]);
+                }
+            }
+            Film.find({status:"coming"},function(err,fic){
+                if (err) return console.log(err);
+            res.render('index',{
+                filmslide:fis,
+                films:newFis,
+                filmc:fic,
+                dateRq:dateRq,
+                dayWeek: dayWeek
+                });
+            })
+        })
 })
 
 
